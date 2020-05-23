4f95e10ed533bda74c925f07ead6f4091281325b
Roman Vyunov <vyunov.rom@mail.ru>
2020-05-23 19:22:02 +0300

    something

---
+++ src/main/java/Shared/FileDescriptor.java
@@ -0,0 +1,24 @@
+package Shared;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileDescriptor {
+    private static Logger log = Logger.getLogger(FileDescriptor.class.getName());
+    String name;
+    long byteSize;
+    byte[] bin;
+    public FileDescriptor(File file) {
+        name = file.getName();
+        byteSize = file.length();
+        try {
+            bin = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "FileDescriptor: ", e);
+        }
+    }
+}
